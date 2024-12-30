9e9c94051fddc17fa1dd51d11c8a72b0b1bfbd02
nidhinsankar <nidhinsankar1@gmail.com>
2024-12-30 16:58:17 +0530

    fix:navbar position is fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 export const Navbar = () => {
   return (
     <motion.div
-      className="flex justify-between items-center sticky top-4 px-3 rounded-2xl opacity-80 w-[760px] mx-auto bg-red-400 h-20"
+      className="flex justify-between items-center sticky top-4 z-30 px-3 rounded-2xl opacity-80 w-[760px] mx-auto bg-red-400 h-20"
       whileHover={{ scale: 1.05 }}
       transition={{ duration: 0.3 }}
     >
